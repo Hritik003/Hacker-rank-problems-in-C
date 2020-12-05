00f25abc9d7e1516dabc77503b29a34d03a63245
Hritik003 <73677045+Hritik003@users.noreply.github.com>
2020-12-05 16:38:27 +0530

    boxes through the tunnel

---
+++ finding the useful volume.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_HEIGHT 41
+int volume(int,int,int);
+int main()
+{
+	int n,i,a,length[20],width[20],height[20];
+	printf("enter the value of n");
+	scanf("%d",&n);
+	for(i=0;i<=n;i++)
+	{
+		scanf("%d %d %d",length[a],width[a],height[a]);
+		
+	}
+	if(height[a]<41)
+	{
+	return volume;
+		
+	}
+	
+}
+
+int volume(int length , int width , int height)
+{
+	int volume;
+	volume=length*width*height;
+	
+}
